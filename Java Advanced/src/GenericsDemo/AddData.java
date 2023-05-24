992d85524b7b24639fecca80abf0507275476fba
Manoj <hmanoj12@gmail.com>
2023-05-24 23:58:28 +0530

    Initial commit

---
+++ Java Advanced/src/GenericsDemo/AddData.java
@@ -0,0 +1,13 @@
+package GenericsDemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class AddData {
+    private int id;
+}
---
+++ Java Advanced/src/GenericsDemo/Runner.java
@@ -0,0 +1,8 @@
+package GenericsDemo;
+
+public class Runner {
+    public static void main(String[] args) {
+        AddData addData = new AddData(1);
+        System.out.println(addData.getId());
+    }
+}
